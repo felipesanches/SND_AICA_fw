12cb6cc0bebbc1df4bee2cff02b1918ed2e6835e
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2015-11-19 20:44:48 -0200

    fixing indentation in aica_cmd_iface.h

--- aica_cmd_iface.h
+++ aica_cmd_iface.h
@@ -9,21 +9,21 @@ typedef unsigned long uint32;
 
 /* Make this 8 dwords long for one aica bus queue */
 typedef struct {
-	uint32		cmd;		/* Command ID		*/
-	uint32		pos;		/* Sample position	*/
-	uint32		length;		/* Sample length	*/
-	uint32		freq;		/* Frequency		*/
-	uint32		vol;		/* Volume 0-255		*/
-	uint32		pan;		/* Pan 0-255		*/
-        uint32		sfmt;	        /* Sound format		*/
-	uint32		flags;	        /* flags		*/
+    uint32 cmd;    /* Command ID      */
+    uint32 pos;    /* Sample position */
+    uint32 length; /* Sample length   */
+    uint32 freq;   /* Frequency       */
+    uint32 vol;    /* Volume 0-255    */
+    uint32 pan;    /* Pan 0-255       */
+    uint32 sfmt;   /* Sound format    */
+    uint32 flags;  /* flags           */
 } aica_channel;
 
 /* Command values */
-#define AICA_CMD_KICK		0x80000000
-#define AICA_CMD_NONE		0
-#define AICA_CMD_START		1
-#define AICA_CMD_STOP		2
-#define AICA_CMD_VOL		3
+#define AICA_CMD_KICK   0x80000000
+#define AICA_CMD_NONE   0
+#define AICA_CMD_START  1
+#define AICA_CMD_STOP   2
+#define AICA_CMD_VOL    3
 
 #endif	/* __ARM_AICA_CMD_IFACE_H */
